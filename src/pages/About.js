15577e64046cf2ea359cccb8a5e81ac07abfadeb
garyongyc <82588368+thegaryong@users.noreply.github.com>
2021-09-17 15:33:00 +0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus rerum in
+        magni facere neque error fugit voluptatibus consequatur suscipit
+        incidunt magnam obcaecati laudantium eius autem facilis alias temporibus
+        ratione ex ad vero, corporis ut perferendis dolor esse! Minima, error
+        eos.
+      </p>
+    </section>
+  );
+};
+
+export default About;
